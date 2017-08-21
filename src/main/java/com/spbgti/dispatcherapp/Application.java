f074b483573b28cd74f067c8a54b7846a24220c4
Daniil <burhaunt@gmail.com>
2017-08-22 02:36:08 +0300

    added dependencies for MongoDB

--- src/main/java/com/spbgti/dispatcherapp/Application.java
+++ src/main/java/com/spbgti/dispatcherapp/Application.java
@@ -3,8 +3,6 @@ package com.spbgti.dispatcherapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @SpringBootApplication
