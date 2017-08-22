a687ad897c41ffc4ab7d79bef238490431752705
Daniil <burhaunt@gmail.com>
2017-08-22 17:43:09 +0300

    changed wrong type of access of dataSource method

--- src/main/java/com/spbgti/dispatcherapp/Configuration/Config.java
+++ src/main/java/com/spbgti/dispatcherapp/Configuration/Config.java
@@ -31,7 +31,7 @@ public class Config {
     }
 
     @Bean
-    private DataSource dataSource() {
+    public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/testdb");
