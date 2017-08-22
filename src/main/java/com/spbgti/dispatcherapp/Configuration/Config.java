7a67fec61796718ca3f58f8b68067f313d73c552
Daniil <burhaunt@gmail.com>
2017-08-23 00:43:41 +0300

    changed loading a properties file

--- src/main/java/com/spbgti/dispatcherapp/Configuration/Config.java
+++ src/main/java/com/spbgti/dispatcherapp/Configuration/Config.java
@@ -1,7 +1,10 @@
 package com.spbgti.dispatcherapp.Configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -18,8 +21,12 @@ import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
+@PropertySource("application.properties")
 @EnableTransactionManagement
 public class Config {
+
+    @Autowired
+    Environment environment;
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
@@ -28,17 +35,17 @@ public class Config {
         em.setPackagesToScan("com.spbgti.dispatcherapp.Entity");
         JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
-        em.setJpaProperties(getProperties());
+        em.setJpaProperties(additionalProperties());
         return em;
     }
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(getProperties().getProperty("spring.datasource.driver"));
-        dataSource.setUrl(getProperties().getProperty("spring.datasource.url"));
-        dataSource.setUsername(getProperties().getProperty("spring.datasource.username"));
-        dataSource.setPassword(getProperties().getProperty("spring.datasource.password"));
+        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver"));
+        dataSource.setUrl(environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(environment.getProperty("spring.datasource.password"));
         return dataSource;
     }
 
@@ -52,6 +59,13 @@ public class Config {
     @Bean
     public PersistenceExceptionTranslationPostProcessor exceptionTranslation() {
         return new PersistenceExceptionTranslationPostProcessor();
+    }
+
+    private Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
+        properties.setProperty("hibernate.dialect", environment.getProperty("hibernate.dialect"));
+        return properties;
     }
 
     private Properties getProperties() {
