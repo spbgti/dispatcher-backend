210d0f4aef244aed7e71aa27927e2a1494fe30fe
Daniil <burhaunt@gmail.com>
2017-10-25 21:17:04 +0300

    changed wrong class (Delete->Update)

--- src/main/java/com/spbgti/dispatcherapp/Entity/Event/Command/Command.java
+++ src/main/java/com/spbgti/dispatcherapp/Entity/Event/Command/Command.java
@@ -7,7 +7,7 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
 @JsonSubTypes({@JsonSubTypes.Type(value = CreateCommand.class, name = "create"),
         @JsonSubTypes.Type(value = DeleteCommand.class, name = "delete"),
-        @JsonSubTypes.Type(value = DeleteCommand.class, name = "update"),
+        @JsonSubTypes.Type(value = UpdateCommand.class, name = "update"),
         @JsonSubTypes.Type(value = MigrationCommand.class, name = "migrate")})
 public interface Command {
     public Object apply() throws Exception;
